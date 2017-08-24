6ff2d29f5deeddc9a52cf214d8aa4bf1b988f31d
Steve Chin <sb_chin@yahoo.com>
2017-08-24 09:55:37 -0400

    exercise3

---
+++ the-basics/exercise3.rb
@@ -0,0 +1,6 @@
+movies = {:rocky => 1975, :stars => 2004, :stripes => 2013, :contact => 2001,:war => 1981}
+puts movies[:rocky]
+puts movies[:stars]
+puts movies[:stripes]
+puts movies[:contact]
+puts movies[:war]
