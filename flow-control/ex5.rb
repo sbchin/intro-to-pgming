5437eac5e93d7b057a16517cc475759c55ad4c1b
Steve Chin <sb_chin@yahoo.com>
2017-10-08 16:17:38 -0400

    ex4 - ex6

---
+++ flow-control/ex5.rb
@@ -0,0 +1,17 @@
+def nbr_msg(number)
+  case 
+  when number < 0 
+    puts "You can't enter a negative number!"
+  when number <= 50
+    puts "#{number} is between 0 and 50"
+  when number <= 100
+    puts "#{number} is between 51 and 100"
+  else
+    puts "#{number} is above 100"
+  end 
+end
+
+puts "Please enter a number between 0 and 100."
+number = gets.chomp.to_i
+
+nbr_msg number
