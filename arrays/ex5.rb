66926635a8ee9745a0e71902d5a5fe1485b0d83f
Steve Chin <sb_chin@yahoo.com>
2017-10-16 09:52:18 -0400

    ex4 - ex7

---
+++ arrays/ex5.rb
@@ -0,0 +1,11 @@
+string = "Welcome to America!"
+a = string[6] 
+b = string[11]
+c = string[19]
+p a, b, c
+
+=begin
+"e"
+"A"
+nil
+=end
---
+++ arrays/ex7.rb
@@ -0,0 +1,7 @@
+nbrs = [1,4,8]
+nbrs_plus2 = nbrs.map do |x|
+  x + 2
+end
+
+p nbrs
+p nbrs_plus2
