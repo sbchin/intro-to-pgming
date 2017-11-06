c51e7492ec0a435678d169fab6a96c7f7a740a13
Steve Chin <sb_chin@yahoo.com>
2017-11-06 11:20:55 -0500

    ex10 - ex13

---
+++ ex10.rb
@@ -0,0 +1,16 @@
+=begin hash value as array
+=end
+
+hash = { names: ["Bob", "Fred", "Susanna"]}
+
+p hash
+
+=begin array of hashes
+=end
+
+persons = [
+  {name: "Bill"},
+  {name: "Emily"}
+]  
+
+p persons
